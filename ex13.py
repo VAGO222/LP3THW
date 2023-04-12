1813612e8baa7d96a384503d6506178bbf600b9a
VAGO222 <130586552+VAGO222@users.noreply.github.com>
2023-04-12 11:11:21 -0600

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+stuff, things, that, third = argv
+
+print("The script is called:", stuff)
+print("Your first variable is:", things)
+print("Your second variable is:", that)
+print("Your third variable is:", third)
